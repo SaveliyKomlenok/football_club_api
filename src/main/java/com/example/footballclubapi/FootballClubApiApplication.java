88238eee6345e368da09b91cfbb9edc223243623
Saveliy Komlenok <skomlenok363@gmail.com>
2025-04-10 15:37:21 +0300

    create project

---
+++ src/main/java/com/example/footballclubapi/FootballClubApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.footballclubapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballClubApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FootballClubApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/footballclubapi/FootballClubApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.footballclubapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballClubApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
